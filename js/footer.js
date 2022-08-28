f1409cc00a47735c4db01634a7d867a355fcf4ef
Stillrain577681223 <95129294+Stillrain577681223@users.noreply.github.com>
2022-08-29 03:07:08 +0800

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,50 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<div class="footer">' +
+    '        <div class="footer_top">' +
+    '              <div class="content_container">' +
+    '                  <div class="footer_jigou">' +
+    '                  <h2>全球办事机构：</h2>' +
+    '                  <a href="jigou.html">暂无，等待开设</a>' +
+    '                  </div>' +
+    '                  <div class="footer_center clearfix">' +
+    '                      <div class="center_left">' +
+    '                          <h2>秋水电玩</h2>' +
+    '                          <p>咨询QQ：1140606039;2863970084;2235415453</p>' +
+    '                          <p><span>总部邮箱：yulongjie@longjieworkgroup.onmicrosoft.com</span></p>' +
+    '                          <p>总部地址：无实体地址</p>' +
+    '                      </div>' +
+    '                      <div class="center_right">' +
+    '                          <h2>2863970084</h2>' +
+    '                          <p>咨询QQ(7/15-8-31每天9:00-21:00,8/31-? 周一至周五 17:50-21:30 周六日 8:00-21:30 节假日同周六日)</p>' +
+    '                      </div>' +
+    '                  </div>' +
+    '              </div>' +
+    '        </div>' +
+    '    <div class="footer_bottom clearfix">' +
+    '        <div class="content_container">' +
+    '            <div class="footer_banquanbeian">' +
+    '                <span>Web Design Copyright By Stillrain© 2022 Ritte Technology Co., Ltd</span>' +
+    '                <a href="javascript:void(0)">沪ICP备13002172号-3</a>' +
+    '                <a href="javascript:void(0)">沪公网安备 31011002002436号</a>' +
+    '            </div>' +
+    '            <div class="footer_link">' +
+    '                <a href="javascript:void(0)">办公系统(无)</a>|' +
+    '                <a href="javascript:void(0)">相关链接(无)</a>|' +
+    '                <a href="javascript:void(0)">免责声明(无)</a>|' +
+    '                <a href="jigou.html">联系我们</a>' +
+    '            </div>' +
+    '        </div>' +
+    '    </div>' +
+    '</div>' +
+    '');
+
+
+
+
+
+
+
+
+
+<!--底部结束-->
---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.write('<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?8c0854c37b23b617c441acb65529fca1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>\n' +
+    '<meta charset="UTF-8">'+
+    '<meta http-equiv="content-type" content="text/html">'+
+    '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>'+
+    '<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">'+
+    '<meta name="renderer" content="webkit">'+
+    '<!--<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">-->'+
+    '<link rel="stylesheet" type="text/css"  href="css/font-awesome-4.7.0/css/font-awesome.min.css">'+
+    '<link rel="stylesheet" type="text/css"  href="css/swiper-bundle.min.css">'+
+    '<link rel="stylesheet" type="text/css" href="css/base.css">'+
+    '<script type="text/javascript" src="js/jquery.js"></script>'+
+    '<script type="text/javascript" src="js/base.js"></script>'+
+    '<script type="text/javascript" src="js/swiper-bundle.min.js"></script>'+
+    '');
+
+
+
+
+
